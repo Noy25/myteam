914ec4788ac07ea0e3db087c29f52272f7568b38
Noy Morgenshtein <noy9630@gmail.com>
2022-02-09 10:27:01 +0200

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// Cmps
+import { Home } from './js/pages/home/Home';
+
+
+export function App() {
+
+
+  return (
+    <section className="main-app">
+      <Home />
+    </section>
+  )
+}
---
+++ src/js/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+
+
+export function Home() {
+
+    return (
+        <main className="home">
+            Home
+        </main>
+    )
+}
